244e0981ffa32019d25012609250961fe52d07ee
Ricardo <ricardovws@gmail.com>
2020-05-08 10:52:39 -0300

    ainda no frontend

--- TechnicalAdvisor/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ TechnicalAdvisor/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -45,7 +45,7 @@ namespace TechnicalAdvisor.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Lembrar a senha?")]
             public bool RememberMe { get; set; }
         }
 
