b6ab28b083e4ecfb6ba3bd618040955407383426
Ricardo <ricardovws@gmail.com>
2020-05-11 16:32:10 -0300

    fazendo pagina do index ainda

---
+++ TechnicalAdvisor/wwwroot/js/main_.js
@@ -0,0 +1,41 @@
+﻿$(".tooltip").tooltipster({
+    theme: "tooltipster-noir"
+});
+
+$("input").focus();
+
+$("#search-button").click(showSearchList);
+$("#search-button_again").click(searchAgain);
+
+
+
+
+function showSearchList() {
+
+    $(".listOfResults").stop().slideToggle();
+    $(".search_box").toggle();
+    //$(".search_box").css("display", "none");
+    var scrollList = $(".navbar-fixed-top").offset().bottom;
+    $("html, body").animate(
+        {
+            scrollTop: scrollList + "px"
+        }, 500
+    )
+    
+    $(".main_search_box").css("height", "20vh");
+    }
+
+function searchAgain() {
+    $(".main_search_box").css("height", "100vh");
+    $(".listOfResults").toggle(500);
+    $(".search_box").toggle(500);
+    //$(".search_box").css("display", "initial");
+    var scrollList = $(".search_box").offset().bottom;
+    $("html, body").animate(
+        {
+            scrollTop: scrollList + "px"
+        }, 500
+    )
+    $("input").val('');
+    $("input").focus();
+}    
